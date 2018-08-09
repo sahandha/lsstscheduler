b9ae9e02120aade001ced9dd7eb37ce5d3a32055
Sahand <sahandha@gmail.com>
2018-08-09 14:55:29 -0500

    fixing bugs :/

--- sched.py
+++ sched.py
@@ -3,15 +3,14 @@ from datetime import datetime, timedelta
 import motor.motor_tornado
 import sys
 
-db = motor.motor_tornado.MotorClient().ResourceAllocation
-
-
-doc = yield db.users.find({},{"_id": 0 ,"username": 1, "cpulimit": 1, "memlimit": 1, "podlimit": 1, "state": 1, "expirationdate": 1}).to_list(length=100)
-userdata = [[l["username"],l["cpulimit"],l["memlimit"],l["podlimit"],l["state"],str(l["expirationdate"]-datetime.now())] for l in doc]
-
-
-print(userdata)
 
 
 if __name__=="__main__":
     print(sys.argv[1])
+
+    db = motor.motor_tornado.MotorClient().ResourceAllocation
+
+    doc = db.users.find({},{"_id": 0 ,"username": 1, "cpulimit": 1, "memlimit": 1, "podlimit": 1, "state": 1, "expirationdate": 1}).to_list(length=100)
+    userdata = [[l["username"],l["cpulimit"],l["memlimit"],l["podlimit"],l["state"],str(l["expirationdate"]-datetime.now())] for l in doc]
+
+    print(userdata)
