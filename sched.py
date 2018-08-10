6a91872c3532b697b569643df62ab39dfca5c343
Sahand <sahandha@gmail.com>
2018-08-10 13:04:20 -0500

    adding service to discover mongo

--- sched.py
+++ sched.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 
 if __name__=="__main__":
     print(sys.argv[1])
-    client = MongoClient('localhost', 27017)
+    client = MongoClient('mongodb', 27017)
     db = client.ResourceAllocation
     doc = db.users.find_one()
     print(doc)
