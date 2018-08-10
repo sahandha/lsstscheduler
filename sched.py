7784cca5237c5c4e07ab838f92d2d67f5489ff3a
Sahand <sahandha@gmail.com>
2018-08-10 09:27:58 -0500

    adding coroutinges

--- sched.py
+++ sched.py
@@ -8,18 +8,26 @@ import motor.motor_tornado
 import sys
 
 
+@gen.coroutine
+def getUserData(db):
+    try:
+        doc = yield db.users.find({},{"_id": 0 ,"username": 1, "cpulimit": 1, "memlimit": 1, "podlimit": 1, "state": 1, "expirationdate": 1}).to_list(length=100)
+        userdata = [[l["username"],l["cpulimit"],l["memlimit"],l["podlimit"],l["state"],str(l["expirationdate"]-datetime.now())] for l in doc]
+    except:
+        userdata = []
+    return userdata
+
+@gen.coroutine
+def printUserData(db)
+    doc = db.users.find({},{"_id": 0 ,"username": 1, "cpulimit": 1, "memlimit": 1, "podlimit": 1, "state": 1, "expirationdate": 1}).to_list(length=100)
+    userdata = [[l["username"],l["cpulimit"],l["memlimit"],l["podlimit"],l["state"],str(l["expirationdate"]-datetime.now())] for l in doc]
+
+    print(userdata)
+
 
 if __name__=="__main__":
     print(sys.argv[1])
 
     db = motor.motor_tornado.MotorClient().ResourceAllocation
 
-    doc = db.users.find({},{"_id": 0 ,"username": 1, "cpulimit": 1, "memlimit": 1, "podlimit": 1, "state": 1, "expirationdate": 1}).to_list(length=100)
-    userdata = [[l["username"],l["cpulimit"],l["memlimit"],l["podlimit"],l["state"],str(l["expirationdate"]-datetime.now())] for l in doc]
-
-    print(userdata)
-
-    print("server running at localhost:8888 ...")
-    ("press ctrl+c to close.")
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+    printUserData(db)
