0f47785771633445a664de536330c912efa8748a
Sahand <sahandha@gmail.com>
2018-08-10 10:49:03 -0500

    using pymongo instead of motor and tornado

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,7 @@ RUN sudo apt-get install -y python-pip python-dev build-essential
 RUN sudo apt-get install -y git
 RUN pip install --upgrade pip
 RUN pip install git+https://github.com/kubernetes-client/python.git
-RUN pip install tornado
-RUN pip install motor
+RUN python -m pip install pymongo
 
 Add kube_deploy.py /kube_deploy.py
 Add sched.py /sched.py
--- sched.py
+++ sched.py
@@ -1,33 +1,14 @@
-import tornado.ioloop
-import tornado.web
-from tornado.web import asynchronous
-from tornado import gen
 import kube_deploy as kd
 from datetime import datetime, timedelta
-import motor.motor_tornado
+import pymongo
 import sys
 
-
-@gen.coroutine
-def getUserData(db):
-    try:
-        doc = yield db.users.find({},{"_id": 0 ,"username": 1, "cpulimit": 1, "memlimit": 1, "podlimit": 1, "state": 1, "expirationdate": 1}).to_list(length=100)
-        userdata = [[l["username"],l["cpulimit"],l["memlimit"],l["podlimit"],l["state"],str(l["expirationdate"]-datetime.now())] for l in doc]
-    except:
-        userdata = []
-    return userdata
-
-@gen.coroutine
-def printUserData(db):
-    doc = db.users.find({},{"_id": 0 ,"username": 1, "cpulimit": 1, "memlimit": 1, "podlimit": 1, "state": 1, "expirationdate": 1}).to_list(length=100)
-    userdata = [[l["username"],l["cpulimit"],l["memlimit"],l["podlimit"],l["state"],str(l["expirationdate"]-datetime.now())] for l in doc]
-
-    print(userdata)
 
 
 if __name__=="__main__":
     print(sys.argv[1])
-
-    db = motor.motor_tornado.MotorClient().ResourceAllocation
-
+    client = MongoClient('localhost', 27017)
+    db = client.ResourceAllocation
+    doc = db.users.find_one()
+    print(doc)
     printUserData(db)
